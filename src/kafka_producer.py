84ffe217a2b16dd6803afc509375c7f8d7a5e78c
AlbaMH01 <a.martinezdel.2025@alumnos.urjc.es>
2026-05-07 18:41:45 +0200

    Producer - carrera real y procesar carrera
    Co-authored-by: Alonso González Romero <alonsogonro@gmail.com>

--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 import json
 import time
 from kafka import KafkaProducer
@@ -7,7 +8,7 @@ from kafka.errors import NoBrokersAvailable
 # =====================================================================
 # CONFIGURACIÓN
 # =====================================================================
-ARCHIVO_CSV = "data/static/carrera_5000m.csv"
+ARCHIVO_CSV = "data/static/simulacion_carrera.csv"
 TOPIC_KAFKA = "race_events"
 FACTOR_VELOCIDAD = 0.1  # 0.1 = x10 de velocidad. 1.0 = Tiempo real.
 
@@ -31,33 +32,46 @@ def enviar_datos_simulados():
     # 👩‍💻👨‍💻 ZONA DE PROGRAMACIÓN EN PAREJA (Vuestro turno)
     # =====================================================================
     
-    # PASO 1: Cargar el CSV
-    # TAREA: Abrid ARCHIVO_CSV, leedlo con DictReader y guardad los eventos en una lista.
-    # PISTA: Recordad convertir 'tiempo_acumulado_s' a float y 'distancia_m' a int.
-    eventos = []
-    
-    
+    # PASO 1: Cargar el CSV'
+    eventos = pd.read_csv(ARCHIVO_CSV)
+    # Cambiamos el tipo de dato de las columnas distancia y tiempo acumulado
+    eventos['distancia_metros'] = eventos['distancia_metros'].astype(int)
+    eventos['tiempo_acumulado_segundos'] = eventos['tiempo_acumulado_segundos'].astype(float)
+
     # PASO 2: Ordenar los datos cronológicamente
-    # TAREA: Aseguraos de que la lista esté ordenada por 'tiempo_acumulado_s'.
+    eventos = eventos.sort_values(by='tiempo_acumulado_segundos').reset_index(drop=True)
     
     
     print(f"📊 Datos listos. Empezando retransmisión...")
     print("-" * 60)
 
     # PASO 3: El Reloj de la Carrera
-    # TAREA: Inicializad una variable 'tiempo_anterior' a 0.0.
-    
+    tiempo_anterior = 0.0
 
     # PASO 4: El Bucle del Directo
-    # TAREA: Recorred la lista de eventos. 
-    # En cada vuelta:
-    #   A) Calculad la diferencia de tiempo con el evento anterior.
-    #   B) Si la diferencia > 0, haced un time.sleep(diferencia * FACTOR_VELOCIDAD).
-    #   C) Enviad el evento a Kafka: producer.send(TOPIC_KAFKA, value=evento).
-    #   D) Actualizad 'tiempo_anterior'.
-    
-    
-    # =====================================================================
+    for i in range(len(eventos)):
+        split = eventos.iloc[i]
+        tiempo = split["tiempo_acumulado_segundos"]
+        diferencia = tiempo - tiempo_anterior
+        if diferencia > 0:
+            time.sleep(diferencia * FACTOR_VELOCIDAD)
+        
+        mensaje = {
+                "schema_version": "1.0",
+                "id_evento": str(split["id_evento"]),
+                "nombre_corredor": str(split["nombre_corredor"]),
+                "timestamp": float(tiempo),
+                "distancia_metros": int(split['distancia_metros'])
+            }
+        
+        producer.send(
+                topic = TOPIC_KAFKA, 
+                #key = player_id.encode('utf-8'), 
+                value = mensaje
+                #timestamp_ms = timestamp
+            )
+        print(f"Enviando: {mensaje['nombre_corredor']} - {mensaje['distancia_metros']}m")
+        tiempo_anterior = tiempo
     
     producer.flush()
     print("-" * 60)
